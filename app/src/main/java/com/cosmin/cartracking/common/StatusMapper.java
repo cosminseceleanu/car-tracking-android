58b1844fcafe4afb87c0ec324a04c35bb7a9b11f
Cosmin Seceleanu <coosmin26@yahoo.com>
2016-12-30 19:38:21 +0200

    add detination activity

---
+++ app/src/main/java/com/cosmin/cartracking/common/StatusMapper.java
@@ -0,0 +1,37 @@
+package com.cosmin.cartracking.common;
+
+
+import com.cosmin.cartracking.R;
+import com.cosmin.cartracking.model.Task;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class StatusMapper {
+    private final static Map<Task.Status, Integer> colors;
+    private final static Map<Task.Status, String> messages;
+
+    static {
+        HashMap<Task.Status, Integer> map = new HashMap<>();
+        map.put(Task.Status.NEW, R.color.colorNew);
+        map.put(Task.Status.FINISHED, R.color.colorDone);
+        map.put(Task.Status.IN_PROGRESS, R.color.colorInProgress);
+        colors = Collections.unmodifiableMap(map);
+    }
+    static {
+        HashMap<Task.Status, String> map = new HashMap<>();
+        map.put(Task.Status.NEW, "Nou");
+        map.put(Task.Status.IN_PROGRESS, "In lucru");
+        map.put(Task.Status.FINISHED, "Finalizat");
+        messages = Collections.unmodifiableMap(map);
+    }
+
+    public static int getColor(Task.Status status) {
+        return colors.get(status);
+    }
+
+    public static String getMessage(Task.Status status) {
+        return messages.get(status);
+    }
+}
